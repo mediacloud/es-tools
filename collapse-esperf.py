a380a9629ba6d1d6c051eceedfafaee1dd5a6824
Phil Budne <phil@regressive.org>
2024-11-20 16:39:10 -0500

    Add _label, LabelContext for safer stack management

--- collapse-esperf.py
+++ collapse-esperf.py
@@ -18,7 +18,7 @@ from stdin.  Always outputs to stdout for piping to flamegraph.pl
 Input data is expected to be JSON returned under "profile", when
 "profile" is set to true in request.
 
-Each "hit" == 1ns
+Each "sample" == 1ns
 Shards are shown as sNN
 Nodes shown as generated node ids.
 
@@ -44,6 +44,19 @@ class Detail(enum.Enum):
     CLUSTER = 4  # show clusters, nodes, indices, shards
 
 
+class LabelContext:
+    def __init__(self, cperf: "CollapseESPerf", label: str):
+        self.cperf = cperf
+        self.label = label
+
+    def __enter__(self) -> "LabelContext":
+        self.cperf._push(self.label)
+        return self
+
+    def __exit__(self, type_: Any, value: Any, traceback: Any) -> None:
+        self.cperf._pop()
+
+
 class CollapseESPerf:
     def __init__(self, detail: Detail, use_descr: bool, breakdown: bool):
         # options, set for all files
@@ -54,11 +67,10 @@ class CollapseESPerf:
         # reset for each shard
         self.stack: list[str] = []
 
-        # summed over all shards;  flamegraphs are usually done on
-        # sampled data, so using the term used in that world, where
-        # "hits" means the number of samples with a given stack trace.
-        # here, each "hit" is in a reported nanosecond.
-        self.hits: Counter[str] = Counter()
+        # summed over all shards in all queries in all files;
+        # flamegraphs are usually done with time based sampling.
+        # Here, each "sample" is in a reported nanosecond of runtime.
+        self.samples: Counter[str] = Counter()
 
     def _reset_stack(self) -> None:
         self.stack = []
@@ -69,25 +81,26 @@ class CollapseESPerf:
     def _pop(self) -> None:
         self.stack.pop()
 
-    def _record_node_and_pop(self, node: dict[str, Any]) -> None:
+    def _record_node(self, node: dict[str, Any]) -> None:
         """
-        record (sampling) hits for a node with time_in_nanos
+        record (sampling) samples for a node with time_in_nanos
         and possibly a "breakdown"
         """
         if self.breakdown and "breakdown" in node:
             for name, nanos in node["breakdown"].items():
                 if nanos:
-                    self._push(name)
-                    self._record_nanos_and_pop(nanos)  # pops detail
-            self._pop()  # pop node id
+                    with self._label(name):
+                        self._record_nanos(nanos)
         else:
-            self._record_nanos_and_pop(node["time_in_nanos"])
+            self._record_nanos(node["time_in_nanos"])
 
-    def _record_nanos_and_pop(self, nanos: int) -> None:
+    def _record_nanos(self, nanos: int) -> None:
         # the one place that formats the stack
         key = ";".join(self.stack)
-        self.hits[key] += nanos
-        self._pop()
+        self.samples[key] += nanos
+
+    def _label(self, name: str) -> LabelContext:
+        return LabelContext(self, name)
 
     def _query(self, qn: dict[str, Any]) -> None:
         # each query node has:
@@ -98,15 +111,16 @@ class CollapseESPerf:
             # causes browser heartburn), and contains both fields
             # AND values which creates noise.  Need to sanitize so
             # field names present, but values are not!
-            self._push(qn["description"][:100])
+            label = qn["description"][:100]
         else:
             # gives VERY dry output (just node types)
-            self._push(qn["type"])
+            label = qn["type"]
 
-        for child in qn.get("children", []):
-            self._query(child)
+        with self._label(label):
+            for child in qn.get("children", []):
+                self._query(child)
 
-        self._record_node_and_pop(qn)
+            self._record_node(qn)
         # end of _query
 
     def _coll(self, cn: dict[str, Any]) -> None:
@@ -114,20 +128,20 @@ class CollapseESPerf:
 
         # reason is "plain english" description of class name
         # name is class name?
-        self._push(cn["reason"])
-        for child in cn.get("children", []):
-            self._coll(child)
-        self._record_node_and_pop(cn)
+        with self._label(cn["reason"]):
+            for child in cn.get("children", []):
+                self._coll(child)
+            self._record_node(cn)
         # end of _coll
 
     def _aggs(self, an: dict[str, Any]) -> None:
         # aggregations nodes have: type, description (agg name)
         #    time_in_nanos, breakdown, debug
 
-        self._push(an["description"])  # aggregation name
-        for child in an.get("children", []):
-            self._aggs(child)
-        self._record_node_and_pop(an)
+        with self._label(an["description"]):  # aggregation name
+            for child in an.get("children", []):
+                self._aggs(child)
+            self._record_node(an)
         # end of _aggs
 
     def collapse(self, stream: TextIO) -> None:
@@ -147,7 +161,8 @@ class CollapseESPerf:
         for shard in p["shards"]:
             self._reset_stack()
 
-            # prepare the foundation:
+            # prepare the foundation, to order.
+            # (should be the only place that calls _push directly).
             if detail.value >= Detail.CLUSTER.value:
                 self._push(shard["cluster"])
             if detail.value >= Detail.NODE.value:
@@ -157,37 +172,33 @@ class CollapseESPerf:
             if detail.value >= Detail.SHARD.value:
                 self._push(f"s{shard['shard_id']}")  # format shard as sNN
 
-            self._push("search")
-            for search in shard["searches"]:  # list
-                """
-                here with dict with 'query', 'rewrite_time', 'collector', 'aggregations'
-                """
+            with self._label("search"):
+                for search in shard["searches"]:  # list
+                    """
+                    here with dict with 'query', 'rewrite_time', 'collector', 'aggregations'
+                    """
 
-                # maybe prepend a digit to rewrite_time, coll, aggs to force the
-                # order they appear in (flamegraph.pl sorts the input by path,
-                # for consistency.
+                    # maybe prepend a digit to rewrite_time, coll, aggs to force the
+                    # order they appear in (flamegraph.pl sorts the input by path,
+                    # for consistency.
 
-                self._push("rewrite")  # add digit?
-                self._record_nanos_and_pop(search["rewrite_time"])
+                    with self._label("rewrite"):  # add digit?
+                        self._record_nanos(search["rewrite_time"])
 
-                self._push("query")  # add digit?
-                for q in search["query"]:  # list
-                    self._query(q)
-                self._pop()  # "query"
+                    with self._label("query"):  # add digit?
+                        for q in search["query"]:  # list
+                            self._query(q)
 
-                self._push("collector")  # add digit?
-                for cn in search["collector"]:  # list
-                    self._coll(cn)
-                self._pop()  # "collector"
-            self._pop()  # "search"
+                    with self._label("collector"):  # add digit?
+                        for cn in search["collector"]:  # list
+                            self._coll(cn)
 
-            self._push("aggregations")
-            for an in shard.get("aggregations", []):  # list
-                self._aggs(an)
-            self._pop()  # "aggregations"
+            with self._label("aggregations"):
+                for an in shard.get("aggregations", []):  # list
+                    self._aggs(an)
 
     def dump(self, output: TextIO) -> None:
-        for key, sum in self.hits.items():
+        for key, sum in self.samples.items():
             output.write(f"{key} {sum}\n")
 
 
